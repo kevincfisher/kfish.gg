328de9a28fa13cf9de0771e5a8f7a6d54a39d047
Kevin Fisher <kevincliffordfisher@gmail.com>
2022-07-24 01:15:27 -0400

    add theme toggle

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,13 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
-		colors: {
-			dark: 'hsl(var(--color-text) / 1)',
-			light: 'hsl(var(--color-bg) / 1)',
-			lightBorder: 'hsl(var(--color-border) / 1)',
-			gradientFrom: 'var(--gradient-from)',
-			gradientTo: 'var(--gradient-to)'
-		},
+		extend:{}
 	},
+	// theme: {
+	// 	colors: {
+	// 		dark: 'hsl(var(--color-text) / 1)',
+	// 		light: 'hsl(var(--color-bg) / 1)',
+	// 		lightBorder: 'hsl(var(--color-border) / 1)',
+	// 		gradientFrom: 'var(--gradient-from)',
+	// 		gradientTo: 'var(--gradient-to)'
+	// 	},
+	// },
 	plugins: [],
+	darkMode:'class'
 }
